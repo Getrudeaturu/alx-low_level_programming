75b096e15f1e49ad46fcdc7599611443a502e6bd
gabbycool <gatrudeaturu@gmail.com>
2022-03-10 10:39:03 -0800

    a program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-/* C program that prints a string with the use of puts function
- * main - entry point
+/** C program that prints a string with the use of puts function
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *   
  */
 
 int main(void)
