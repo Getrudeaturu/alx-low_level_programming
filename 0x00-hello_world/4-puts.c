4af0648ddb27cbbd9e6d9bedb6431b9e158f1d72
gabbycool <gatrudeaturu@gmail.com>
2022-03-10 10:31:08 -0800

    a program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/* C program that prints a string with the use of puts function */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
