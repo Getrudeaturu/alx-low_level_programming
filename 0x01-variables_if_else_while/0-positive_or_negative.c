81d38773acf91c5f396ff83147d1173d6a171525
gabbycool <gatrudeaturu@gmail.com>
2022-03-11 17:17:35 -0800

    print if var is positive or negavtive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Determines if a number is positive, negative or zero.
  *
- * Return : 0 Always (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
