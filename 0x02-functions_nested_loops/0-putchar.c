09024434fbf6aa5cf83d8dd732b2ccd667cf1a65
gabbycool <gatrudeaturu@gmail.com>
2022-03-15 19:24:19 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line.
+ *
+ * return 0
+ */
+int main(void)
+{
+	int i;
+
+	char test[9] = "_putchar";
+
+	for (i = 0, i < 8, i++)
+	{
+		_putchar(test[i]);
+	}
+	_putchar('\n');
+
+	return 0;
+}
